d65238283cf581f8894f2c031011c31617b6a306
Pablo Rogers <realpablorogers@gmail.com>
2024-12-10 16:04:39 -0500

    refactor: moved forgot password error handling to toast promise

--- src/features/auth/hooks/useForgotPassword.tsx
+++ src/features/auth/hooks/useForgotPassword.tsx
@@ -9,6 +9,7 @@ import { toast } from "sonner";
 
 import { isAuthApiError } from "@supabase/supabase-js";
 import { z } from "zod";
+import getURL from "../utils/getURL";
 import handleAuthErrors from "../utils/handleAuthErrors";
 
 export default function useForgotPassword() {
@@ -17,44 +18,49 @@ export default function useForgotPassword() {
   const handleResetPasswordForEmail = async (
     data: z.infer<typeof ForgotPasswordFormStep1Schema>,
   ) => {
-    try {
-      const { error } = await supabase.auth.resetPasswordForEmail(data.email, {
-        redirectTo: "http://localhost:3000/forgot-password/update",
-      });
+    toast.promise(
+      supabase.auth.resetPasswordForEmail(data.email, {
+        redirectTo: getURL(),
+      }),
+      {
+        loading: "Sending password reset email...",
+        success: ({ error }) => {
+          if (error) throw error;
 
-      if (error) throw error;
-
-      toast.info(
-        "We’ve sent a password reset link to your email. Please check your inbox and follow the instructions to reset your password. If you don’t see the email, check your spam or junk folder.",
-        { duration: 15000 },
-      );
-    } catch (error) {
-      if (isAuthApiError(error)) {
-        handleAuthErrors(error);
-      } else {
-        toast.error("An unexpected error occurred. Please try again.");
-      }
-    }
+          return `A password reset link has been sent to ${data.email}. Check your inbox or spam folder to proceed.`;
+        },
+        error: (error) => {
+          if (isAuthApiError(error)) {
+            return handleAuthErrors(error);
+          }
+          return "An unexpected error occurred. Please try again.";
+        },
+      },
+    );
   };
 
   const handleUpdatePassword = async (
     data: z.infer<typeof UpdatePasswordSchema>,
   ) => {
-    try {
-      const { error } = await supabase.auth.updateUser({
+    toast.promise(
+      supabase.auth.updateUser({
         password: data.password,
-      });
+      }),
+      {
+        loading: "Updating password...",
+        success: ({ error }) => {
+          if (error) throw error;
 
-      if (error) throw error;
-
-      toast.success("Password updated successfully!");
-    } catch (error) {
-      if (isAuthApiError(error)) {
-        handleAuthErrors(error);
-      } else {
-        toast.error("An unexpected error occurred. Please try again.");
-      }
-    }
+          return "Password updated successfully!";
+        },
+        error: (error) => {
+          if (isAuthApiError(error)) {
+            return handleAuthErrors(error);
+          }
+          return "An unexpected error occurred. Please try again.";
+        },
+      },
+    );
   };
 
   return { handleResetPasswordForEmail, handleUpdatePassword };
