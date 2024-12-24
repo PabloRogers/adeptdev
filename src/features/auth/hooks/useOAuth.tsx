473c17f32fde9efe47cd6983d26b0caf81c8dc50
Pablo Rogers <realpablorogers@gmail.com>
2024-12-23 20:50:27 -0600

    refactor: move OAuth logic to try catch

--- src/features/auth/hooks/useOAuth.tsx
+++ src/features/auth/hooks/useOAuth.tsx
@@ -3,7 +3,6 @@
 import createClient from "@/utils/supabase/client";
 import { isAuthApiError } from "@supabase/supabase-js";
 import { useState } from "react";
-import { toast } from "sonner";
 import { OAuthProvider } from "../types/OAuthProvider";
 import handleAuthErrors from "../utils/handleAuthErrors";
 
@@ -11,32 +10,24 @@ export default function useOAuth(provider: OAuthProvider) {
   const supabase = createClient();
   const [isLoading, setIsLoading] = useState(false);
 
-  async function signInWithOAuthProvider() {
-    const response = await supabase.auth.signInWithOAuth({
-      provider,
-      options: {
-        redirectTo: "http://localhost:3000/callback",
-      },
-    });
-
-    return response;
-  }
-
   async function handleSignInWithOAuthProvider() {
-    setIsLoading(true);
-    toast.promise(signInWithOAuthProvider(), {
-      loading: "Logging in...",
-      success: ({ error }) => {
-        if (error) throw error;
-        return "Login successful!";
-      },
-      error: (error) => {
-        if (isAuthApiError(error)) {
-          return handleAuthErrors(error);
-        }
-        return "An unexpected error occurred. Please try again.";
-      },
-    });
+    try {
+      setIsLoading(true);
+      const response = await supabase.auth.signInWithOAuth({
+        provider,
+        options: {
+          redirectTo: "http://localhost:3000/callback",
+        },
+      });
+      return response;
+    } catch (error) {
+      setIsLoading(false);
+      if (isAuthApiError(error)) {
+        return handleAuthErrors(error);
+      }
+      return "An unexpected error occurred. Please try again.";
+    }
   }
+
   return { handleSignInWithOAuthProvider, isLoading };
 }
