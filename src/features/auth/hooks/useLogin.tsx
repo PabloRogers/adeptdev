23f1a49be688784daff5ec8721d071e1d607eff1
Pablo Rogers <realpablorogers@gmail.com>
2024-12-23 19:31:19 -0600

    refactor: move auth logic to its own function to be called in the toast promise

--- src/features/auth/hooks/useLogin.tsx
+++ src/features/auth/hooks/useLogin.tsx
@@ -3,33 +3,38 @@
 import LoginFormSchema from "@/features/auth/types/login";
 import createClient from "@/utils/supabase/client";
 import { isAuthApiError } from "@supabase/supabase-js";
+import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 import z from "zod";
 import handleAuthErrors from "../utils/handleAuthErrors";
 
 export default function useLogin() {
   const supabase = createClient();
+  const router = useRouter();
+
+  async function signUp(formData: z.infer<typeof LoginFormSchema>) {
+    const signUpResponse = await supabase.auth.signInWithPassword({
+      email: formData.email,
+      password: formData.password,
+    });
+    router.refresh();
+    return signUpResponse;
+  }
 
   async function handleSignUp(formData: z.infer<typeof LoginFormSchema>) {
-    toast.promise(
-      supabase.auth.signInWithPassword({
-        email: formData.email,
-        password: formData.password,
-      }),
-      {
-        loading: "Logging in...",
-        success: ({ error }) => {
-          if (error) throw error;
-          return "Login successful!";
-        },
-        error: (error) => {
-          if (isAuthApiError(error)) {
-            return handleAuthErrors(error);
-          }
-          return "An unexpected error occurred. Please try again.";
-        },
+    toast.promise(signUp(formData), {
+      loading: "Logging in...",
+      success: ({ error }) => {
+        if (error) throw error;
+        return "Login successful!";
       },
-    );
+      error: (error) => {
+        if (isAuthApiError(error)) {
+          return handleAuthErrors(error);
+        }
+        return "An unexpected error occurred. Please try again.";
+      },
+    });
   }
 
   return { handleSignUp };
--- src/features/auth/hooks/useRegister.tsx
+++ src/features/auth/hooks/useRegister.tsx
@@ -2,36 +2,37 @@
 
 import createClient from "@/utils/supabase/client";
 import { isAuthApiError } from "@supabase/supabase-js";
-
 import { toast } from "sonner";
 import handleAuthErrors from "../utils/handleAuthErrors";
 
 export default function useRegister() {
   const supabase = createClient();
 
-  const handleSignUp = async (email: string, password: string) => {
-    toast.promise(
-      supabase.auth.signUp({
-        email,
-        password,
-      }),
-      {
-        loading: "Sending confirmation email...",
-        success: ({ error }) => {
-          if (error) throw error;
+  async function signUp(email: string, password: string) {
+    const signUpResponse = await supabase.auth.signUp({
+      email,
+      password,
+    });
+    return signUpResponse;
+  }
 
-          return `A confirmation link has been sent to ${email}. Check your inbox or spam folder to proceed.`;
-        },
-        error: (error) => {
-          if (isAuthApiError(error)) {
-            return handleAuthErrors(error);
-          }
-          return "An unexpected error occurred. Please try again.";
-        },
-        duration: 30000,
+  async function handleSignUp(email: string, password: string) {
+    toast.promise(signUp(email, password), {
+      loading: "Sending confirmation email...",
+      success: ({ error }) => {
+        if (error) throw error;
+
+        return `A confirmation link has been sent to ${email}. Check your inbox or spam folder to proceed.`;
       },
-    );
-  };
+      error: (error) => {
+        if (isAuthApiError(error)) {
+          return handleAuthErrors(error);
+        }
+        return "An unexpected error occurred. Please try again.";
+      },
+      duration: 30000,
+    });
+  }
 
   return { handleSignUp };
 }
