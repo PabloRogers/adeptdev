12d36c93d930a3446031a4bd3dc11a9ab01b1597
Pablo Rogers <realpablorogers@gmail.com>
2024-12-24 16:19:17 -0600

    feat: removes padding on auth form on smaller breakpoints

--- src/features/auth/components/FormContentWrapper.tsx
+++ src/features/auth/components/FormContentWrapper.tsx
@@ -10,12 +10,12 @@ export default function FormContentWrapper({
   return (
     <div className="flex h-full w-full items-center justify-center">
       <div className="flex h-screen w-full flex-col items-center justify-center lg:flex-row">
-        <div className="hidden h-full w-full items-center justify-center px-2 py-10 lg:flex lg:w-1/2">
+        <div className="hidden h-full w-full items-center justify-center px-2 lg:flex lg:w-1/2 lg:py-10">
           <Card className="flex h-full max-h-[800px] w-full flex-grow items-center justify-center rounded-lg">
             hero
           </Card>
         </div>
-        <div className="flex h-full w-full max-w-xl items-center justify-center px-2 py-10 lg:w-1/2">
+        <div className="flex h-full w-full max-w-xl items-center justify-center px-2 lg:w-1/2 lg:py-10">
           <Card className="flex h-full max-h-[800px] w-full items-center justify-center rounded-lg">
             {children}
           </Card>
