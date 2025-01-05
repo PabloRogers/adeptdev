12f244c21518b570b8ab7ed59b1ee9ad2139d03d
Pablo Rogers <realpablorogers@gmail.com>
2025-01-05 16:09:48 -0600

    revert: remove url toast error

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,19 +1,10 @@
 import FormContentWrapper from "@/features/auth/components/FormContentWrapper";
-import URLErrorToast from "@/features/auth/components/URLErrorToast";
 import LoginForm from "@/features/auth/components/login/LoginForm";
 
-export default function page({
-  searchParams,
-}: {
-  searchParams: { error?: string };
-}) {
-  const errorCode = searchParams.error || null;
+export default function page() {
   return (
-    <>
-      <URLErrorToast error={errorCode} />
-      <FormContentWrapper>
-        <LoginForm />
-      </FormContentWrapper>
-    </>
+    <FormContentWrapper>
+      <LoginForm />
+    </FormContentWrapper>
   );
 }
