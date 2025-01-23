cc1d7c0c4f6eebf3795abd9b2382f80393653064
Pablo Rogers <realpablorogers@gmail.com>
2025-01-23 15:54:36 -0500

    refactor: change hero text to image icon

--- src/features/auth/components/FormContentWrapper.tsx
+++ src/features/auth/components/FormContentWrapper.tsx
@@ -1,4 +1,5 @@
 import { Card } from "@/components/ui/card";
+import { Image } from "react-feather";
 
 interface FormContentWrapperProps {
   children: React.ReactNode;
@@ -12,7 +13,7 @@ export default function FormContentWrapper({
       <div className="flex h-screen w-full flex-col items-center justify-center lg:flex-row">
         <div className="hidden h-full w-full items-center justify-center px-2 lg:flex lg:w-1/2 lg:py-10">
           <Card className="flex h-full w-full items-center justify-center rounded-lg">
-            hero
+            <Image color="#595959" />
           </Card>
         </div>
         <div className="flex h-full w-full max-w-xl items-center justify-center px-2 lg:w-1/2 lg:py-10">
